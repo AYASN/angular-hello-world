c19b0ea1191b686cdc22d072593e504d1cc5e41f
AYassine <assiryassine@gmail.com>
2020-08-22 12:51:44 +0200

    Bind class elements with many interpolation cases

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,36 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <h2>{{2+5}}</h2>
+    <h2>{{"Welcome " + name}}</h2>
+    {{name.length}}
+    {{name.toUpperCase()}}
+    {{greetUser()}}<br>
+    <h3>{{siteUrl}}</h3>
+    <input [id]="myId" type="text" value="Angular">
+    <input id="{{myId}}" [disabled]="isDisabled" type="text" value="Angular">
+    <input id="{{myId}}" bind-disabled="isDisabled" type="text" value="Angular">
+  `,
+  styleUrls: []
 })
 export class TestComponent implements OnInit {
+
+  public name = 'User';
+  public myId = 'testId';
+  public isDisabled = true;
+  public siteUrl = window.location.href;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser(){
+    return 'Hello ' + this.name;
   }
 
 }
