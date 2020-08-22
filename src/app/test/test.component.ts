27cb4b242eb85d5f6099204b6e532bc2fa7e4517
AYassine <assiryassine@gmail.com>
2020-08-22 20:37:31 +0200

    Handling class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,20 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
+    <h2 class="text-success">
       Welcome {{name}}
     </h2>
-    <h2>{{2+5}}</h2>
+    <h2 [class] = "successClass">{{2+5}}</h2>
+    <h3 class="text-special" [class] = "successClass">{{siteUrl}}</h3>
+    <h2 [class.text-danger]="hasError"></h2>
+    <h2 [ngClass]="messageClasses" >Object of Classes example</h2>
+
     <h2>{{"Welcome " + name}}</h2>
     {{name.length}}
     {{name.toUpperCase()}}
     {{greetUser()}}<br>
-    <h3>{{siteUrl}}</h3>
     <input [id]="myId" type="text" value="Angular">
     <input id="{{myId}}" [disabled]="isDisabled" type="text" value="Angular">
     <input id="{{myId}}" bind-disabled="isDisabled" type="text" value="Angular">
   `,
-  styleUrls: []
+  styles: [`
+    .text-success {
+      color: green;
+    }
+    .text-danger {
+      color : red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
@@ -24,6 +37,15 @@ export class TestComponent implements OnInit {
   public myId = 'testId';
   public isDisabled = true;
   public siteUrl = window.location.href;
+
+  public successClass = 'text-success';
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    'test-success': !this.hasError,
+    'test-danger': this.hasError,
+    'test-special': this.isSpecial
+  };
 
   constructor() { }
 
