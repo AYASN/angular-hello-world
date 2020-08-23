f84bb30f7b8892f3e92705b86ae76e7b2765b9a4
AYassine <assiryassine@gmail.com>
2020-08-23 12:14:15 +0200

    Implement parent and child components interaction using @Input, @Output and event

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  templateUrl: './child.component.html',
+  template: `
+
+<!--    <h2>{{"Hello " + parentData}}</h2>-->
+    <h2>{{"Hello " + name}}</h2>
+    <button (click)="fireEvent()">Send event to parent component</button>
+
+  `,
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+
+  // @Input() public parentData;
+
+  // or we can use alias
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fireEvent() {
+    this.childEvent.emit('Hey codeval');
+  }
 }
